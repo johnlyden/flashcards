ec1932a52d45a57719fbf897e1fb5e2b20d157c1
johnlyden <john.lyden86@gmail.com>
2017-10-31 09:41:59 -0500

    removed unused methods and added comments to the guesses controller and card model

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,76 +3,52 @@ require 'pry'
 class GuessesController < ApplicationController
   before_action :set_guess, only: [:show, :edit, :update, :destroy]
 
-  # GET /guesses
-  # GET /guesses.json
-  def index
-    @guesses = Guess.all
-  end
-
-  # GET /guesses/1
-  # GET /guesses/1.json
-  def show
-  end
-
-  # GET /guesses/new
-  def new
-    @guess = Guess.new
-  end
-
-  # GET /guesses/1/edit
-  def edit
-  end
-
   # POST /guesses
-  # POST /guesses.json
+  # when you answer a question - this is where the form POSTs to
+  # this method will create a guess object with the information we need, then it will check the guess, and if the guess is correct it will remove the card from our array of cards we need to answer
   def create
+    # create the guess object based on the params passed form the form, i.e the text of the guess, the user_id and the card_id
+    # we are able to find the @card and the @round becuase we passed their id's as hidden fields in the form that was submitted
     @guess = Guess.create(guess_params)
+    # find the card the guess is associated with
     @card = Card.find(guess_params[:card_id]);
+    # find the round the guess is associated with
     @round = Round.find(guess_params[:round_id])
 
     respond_to do |format|
-
+      # calling the card instance method 'check_guess' and passing it the @guess object
+      # this method will compare the guess to the card's answer and return true if correct, false if incorrect
+      # you can find this check_guess method in models/card.rb
       result = @card.check_guess(@guess)
       
+      # if the result of the guess was true, that means it was correct
       if result == true
+        # the guess was correct, so update the "correct" attribute on the guess to true (it was false by default)
         @guess.update_attributes(correct: true)
+        # delete the card_id that was answered correctly from our array of cards_not_answered, which is stored in the session
+        # if we delete the card_id of the card that was answered, it will no longer be showin in the game
         session[:cards_not_answered].delete(@card.id)
 
+        # this is checking if there aren't any cards left in the cards_not_answered
+        # if its greater than 0, then there are still cars left to anser
         if session[:cards_not_answered].length > 0
-          
+          # randomly select a card_id from the cards_not_answered array
           next_card_id = session[:cards_not_answered].sample
+          # redirect to play that card, using the variable next_card_id that we just made
+          # this is redirecting to the /cards/:id/play/:round_id route, which will trigger the play_card method.  the play_card method will render the view with the new card's question and a submit form to anser it
           format.html {redirect_to "/cards/#{next_card_id}/play/#{@round.id}"}
         else
+          # if there aren't any cards left in the cards_not_answered array then the game must be over - we have answered all the cards
+          # in this case, we should redirect to the results route which will show our results
           format.html {redirect_to "/rounds/#{@round.id}/results"}
         end
       else
-          next_card_id = session[:cards_not_answered].sample
-          format.html {redirect_to "/cards/#{next_card_id}/play/#{@round.id}"} 
+        # down here is the block for if the guess was not correct
+        # if the guess was incorrect, we simply pick a new card_id from the cards_not_answered array that is stored in the session
+        next_card_id = session[:cards_not_answered].sample
+        # with that next_card_id we can redirect to /cards/:id/play/:round_id route, which will trigger the play_card method which will render the view with the next card's question and a submit form to answer
+        format.html {redirect_to "/cards/#{next_card_id}/play/#{@round.id}"} 
       end
-    end
-  end
-
-  # PATCH/PUT /guesses/1
-  # PATCH/PUT /guesses/1.json
-  def update
-    respond_to do |format|
-      if @guess.update(guess_params)
-        format.html { redirect_to @guess, notice: 'Guess was successfully updated.' }
-        format.json { render :show, status: :ok, location: @guess }
-      else
-        format.html { render :edit }
-        format.json { render json: @guess.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /guesses/1
-  # DELETE /guesses/1.json
-  def destroy
-    @guess.destroy
-    respond_to do |format|
-      format.html { redirect_to guesses_url, notice: 'Guess was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/models/card.rb
+++ app/models/card.rb
@@ -4,10 +4,12 @@ class Card < ActiveRecord::Base
     belongs_to :deck
     has_many :guesses
 
+    # instance method for Card that will take a guess object and check if the guess matches the card's answer
     def check_guess(guess)
         # check the guess.text against the card(self) answer
+        # downcase both the guess and the answer so case won't matter
         if guess.text.downcase == self.answer.downcase
-            guess.update_attributes(correct: true);
+            # return true so we can store the result of the check in a variable back in our guesses_controller.rb
             return true
         else
             return false
